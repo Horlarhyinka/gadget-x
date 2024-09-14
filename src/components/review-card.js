558b2819bd2e443b68330cfe56350672be2f894d
Horlarhyinka <danijufarouq2003@gmail.com>
2024-09-14 20:22:13 +0100

    cleaned up envs

--- src/components/review-card.js
+++ src/components/review-card.js
@@ -2,11 +2,10 @@ import React from "react";
 import "./styles/review-card.css"
 
 const ReviewCard = ({userName, avatar, comment})=><li className="review-card" >
-    <img src={avatar}/>
-    <div>
+    <div className="review-card-content" >
     <h1 className="username">{userName}</h1>
     <p>{comment}</p>
-    <div style={{fontSize: "16px", paddingLeft: "0px", color: "yellow"}} >&#9733;&#9733;&#9733;&#9733;&#9733;</div>
+    <div style={{fontSize: "16px", paddingLeft: "0px", color: "rgb(255, 174, 0)"}} >&#9733;&#9733;&#9733;&#9733;&#9733;</div>
     </div>
 </li>
 
